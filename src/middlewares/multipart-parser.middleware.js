8f014c57346b7309f36d84806518cc5080833773
anjal <anjalthapa578@gmail.com>
2025-03-08 12:31:37 +0545

    configuring multer

---
+++ src/middlewares/multipart-parser.middleware.js
@@ -0,0 +1,43 @@
+const multer = require("multer")
+
+const storage = multer.diskStorage({
+  destination: (req,file,cb) => {
+    cb(null, "./public/")
+  },
+  filename: (req,file,cb) => {
+    cb(null, file.originalname)
+  }
+})
+
+const upload = (type = 'image') => {
+
+  let allowed = []
+
+  if (type === 'image') {
+    allowed = ['jpg','jpeg','png','gif','svg','webp', 'bmp']
+  } else if (type === 'doc') {
+    allowed = ['doc','pdf','ppt','csv','xlsx','txt','json']
+  }
+  
+  return multer({
+    storage: storage,
+    fileFilter: (req,file,cb) => {
+      let ext = file.originalname.split(".").pop()
+      if (allowed.includes(ext)) {
+        cb(null, true)
+      } else {
+        cb({
+          code: 400,
+          message: "File format not supported",
+          status: "INVALID_FILE_FORMAT",
+          detail: "" // come back to this later on in the project
+        })
+      }
+    },
+    limits: {
+      fileSize: 3000000
+    }
+  })
+}
+
+module.exports = upload
