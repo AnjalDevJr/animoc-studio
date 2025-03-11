6d7064944a16e4f5e523ff27420598dcd3dc74dc
anjal <anjalthapa578@gmail.com>
2025-03-11 23:35:33 +0545

    binding body parsers in express configuration

--- src/config/express.config.js
+++ src/config/express.config.js
@@ -2,6 +2,12 @@ const express = require("express")
 const animocRouter = require("../router/router.js")
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({
+  extended:false
+}))
+
 app.use('/animoc/v1', animocRouter)
 
 module.exports = app
