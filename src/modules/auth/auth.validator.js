1ceb16843fef2954592a3528a4d1af8ecc223cf5
anjal <anjalthapa578@gmail.com>
2025-03-15 18:30:48 +0545

    added custom messages for error handling

--- src/modules/auth/auth.validator.js
+++ src/modules/auth/auth.validator.js
@@ -1,13 +1,29 @@
 const Joi = require("joi")
 
 const registerDTO = Joi.object({
-  name: Joi.string().min(2).max(50).required(),
-  email: Joi.string().required(),
-  password: Joi.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%&*_-])[a-zA-Z\d!@#$%&*_-]{8,15}$/).required(),
-  confirmPassword: Joi.string().equal(Joi.ref('password')).required(),
-  country: Joi.string().required(),
-  phone: Joi.string().required(),
-  gender: Joi.string().regex(/^(male|female)$/).optional(),
+  name: Joi.string().min(2).max(50).required().messages({
+    "string.empty": "Name should not be empty"
+  }),
+  email: Joi.string().required().messages({
+    "string.empty": "Email should not be empty"
+  }),
+  password: Joi.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%&*_-])[a-zA-Z\d!@#$%&*_-]{8,15}$/).required().messages({
+    "string.empty": "Password should not be empty",
+    "string.pattern.base": "Password should contain atleast one Capital Letter,one Small Letter, one digit and a special Character"
+  }),
+  confirmPassword: Joi.string().equal(Joi.ref('password')).required().messages({
+    "string.empty": "confirmPassword should not be empty",
+    "any.only": "confirmPassword does not match password" 
+  }),
+  country: Joi.string().required().messages({
+    "string.empty": "country should not be empty"
+  }),
+  phone: Joi.string().required().messages({
+    "string.empty": "phone number should not be empty"
+  }),
+  gender: Joi.string().regex(/^(male|female)$/).optional().messages({
+    "string.pattern.base": "Gender should be either male or female"
+  }),
   image: Joi.any().optional()
 })
 
