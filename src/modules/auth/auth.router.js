e909e9e3c3e92db7921d355bdc5439634bb70f07
anjal <anjalthapa578@gmail.com>
2025-03-21 23:30:44 +0545

    routing for auth module

--- src/modules/auth/auth.router.js
+++ src/modules/auth/auth.router.js
@@ -1,3 +1,15 @@
+const checkLogin = require("../../middlewares/auth.middleware")
+const bodyValidator = require("../../middlewares/bodyValidator.middleware")
+const upload = require("../../middlewares/multipart-parser.middleware")
+const authCtrl = require("./auth.controller")
+const {registerDTO,activateDTO,loginDTO} = require("./auth.validator")
+
 const authRouter = require("express").Router()
+
+authRouter.post('/register', upload().single('image'), bodyValidator(registerDTO), authCtrl.registerUser)
+authRouter.post('/activate',bodyValidator(activateDTO), authCtrl.activateUser)
+authRouter.post('/resend-otp', authCtrl.resendOTP)
+authRouter.post('/login', bodyValidator(loginDTO), authCtrl.login)
+authRouter.get('/me', checkLogin, authCtrl.getLoggedInUser)
 
 module.exports = authRouter
