28eea37dbe3dcd8b6200f92f4ea2915b1ff7a361
anjal <anjalthapa578@gmail.com>
2025-04-11 20:31:58 +0545

    added cors to project

--- src/config/express.config.js
+++ src/config/express.config.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const animocRouter = require("../router/router.js")
+const cors = require("cors")
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({
