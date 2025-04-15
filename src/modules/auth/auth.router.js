e8fb857bd8944e5d7f7300d7afd1f0bfe986799b
anjal <anjalthapa578@gmail.com>
2025-04-15 15:34:31 +0545

    final auth router module

--- src/modules/auth/auth.router.js
+++ src/modules/auth/auth.router.js
@@ -1,15 +1,22 @@
-const checkLogin = require("../../middlewares/auth.middleware")
+const {checkLogin, checkRefreshToken} = require("../../middlewares/auth.middleware")
 const bodyValidator = require("../../middlewares/bodyValidator.middleware")
 const upload = require("../../middlewares/multipart-parser.middleware")
 const authCtrl = require("./auth.controller")
-const {registerDTO,activateDTO,loginDTO} = require("./auth.validator")
+const {registerDTO,activateDTO,loginDTO, forgotPasswordDTO, verifyOtpDTO, resetPasswordDTO} = require("./auth.validator")
 
 const authRouter = require("express").Router()
 
 authRouter.post('/register', upload().single('image'), bodyValidator(registerDTO), authCtrl.registerUser)
 authRouter.post('/activate',bodyValidator(activateDTO), authCtrl.activateUser)
 authRouter.post('/resend-otp', authCtrl.resendOTP)
+
 authRouter.post('/login', bodyValidator(loginDTO), authCtrl.login)
+
 authRouter.get('/me', checkLogin, authCtrl.getLoggedInUser)
+authRouter.get('/refresh', checkRefreshToken, authCtrl.getRefreshToken)
+
+authRouter.post('/forgot-password', bodyValidator(forgotPasswordDTO), authCtrl.forgotPassword)
+authRouter.post('/verify-otp', bodyValidator(verifyOtpDTO) , authCtrl.verifyOTP)
+authRouter.post('/reset-password', bodyValidator(resetPasswordDTO), authCtrl.resetPassword)
 
 module.exports = authRouter
